ff59ae7ea0297b8a8516126446fa784b6f86621e
Jung Yuil <yi1397a@gmail.com>
2020-03-26 15:15:25 +0900

    200326

---
+++ camera_control.h
@@ -0,0 +1,17 @@
+#ifndef CAMERA_CONTROL_H
+#define CAMERA_CONTROL_H
+
+#endif // CAMERA_CONTROL_H
+
+#define CAM_MODE CAM_320_240
+
+#define NO_CAM_TEST 0
+#define CAM_320_240 320640
+#define CAM_640_480 640480
+
+#include <opencv2/opencv.hpp>
+
+cv::Mat camera_input
+{
+
+}
---
+++ eel_detect.h
@@ -0,0 +1,7 @@
+#ifndef EEL_DETECT_H
+#define EEL_DETECT_H
+
+#endif // EEL_DETECT_H
+
+#include <opencv2/opencv.hpp>
+
